a2ba84d12ccae5a2cb435f43fe99be46d749db92
Xai <sif332@gmail.com>
2024-03-06 14:22:50 +0700

    leaflet

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
-import Map from "react-map-gl";
+import MapComponent from "./components/MapComponent";
+import 'leaflet/dist/leaflet.css';
+
 
 function App() {
   return (
-    <div className="w-[500px] h-[500px] border border-red-500">
-      <Map
-        mapboxAccessToken="pk.eyJ1Ijoid29yYXdhdGUiLCJhIjoiY2xubXpmMjVuMDE4NzJybGp3Zjc0YjJudSJ9.lMMtWaTf7t_bIzyghIqTTA"
-        initialViewState={{
-          longitude: -122.4,
-          latitude: 37.8,
-          zoom: 14,
-        }}
-        style={{ width: "100%", height: "100%" }}
-        mapStyle="mapbox://styles/mapbox/streets-v9"
-      />
+    <div>
+      <h1>React Leaflet Example</h1>
+      <MapComponent position={[51.505, -0.09]} />
     </div>
   );
 }
